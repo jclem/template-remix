f7b0ef272a19ffc20d640a02e66174bce38e692e
Jonathan Clem <jonathan@jclem.net>
2021-11-23 12:42:47 -0500

    Add basic template setup

---
+++ remix.config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+/**
+ * @type {import('@remix-run/dev/config').AppConfig}
+ */
+module.exports = {
+  appDirectory: 'app',
+  browserBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'build',
+  devServerPort: 8002
+}
